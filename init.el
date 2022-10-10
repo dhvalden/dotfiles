86239286cca63b9d24997bebbf2451d04d8223db
Daniel Valdenegro Ibarra <dhvalden@gmail.com>
2022-10-10 12:46:11 -0300

    Add files via upload

--- init.el
+++ init.el
@@ -5,12 +5,12 @@
 ;; https://emacs.stackexchange.com/a/2989
 (setq package-archives
       '(("elpa"     . "https://elpa.gnu.org/packages/")
-        ("melpa-stable" . "https://stable.melpa.org/packages/")
-        ("melpa"        . "https://melpa.org/packages/"))
+	("melpa" . "https://melpa.org/packages/")
+	("melpa-stable" . "https://stable.melpa.org/packages/"))
       package-archive-priorities
-      '(("melpa-stable" . 10)
+      '(("melpa" . 10)
         ("elpa"     . 5)
-        ("melpa"        . 0)))
+	("melpa-stable" . 0)))
 
 (package-initialize)
 
@@ -26,30 +26,27 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-gruvbox t)
-
+  (load-theme 'doom-palenight t) ; doom-palenight doom-ayu-light doom-nord doom-miramare doom-rouge doom-gruvbox doom-moonlight
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
-  
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-neotree-config)
   ;; or for treemacs users
   (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
   (doom-themes-treemacs-config)
-  
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(elpy-modules
-   (quote
-    (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults)))
+   '(elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults))
  '(package-selected-packages
-   (quote
-    (doom-modeline all-the-icons elpy rust-mode magit ess solaire-mode use-package doom-themes))))
+   '(yaml-mode treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs doom-modeline all-the-icons elpy rust-mode magit ess solaire-mode use-package doom-themes)))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -58,28 +55,11 @@
  )
 
 (require 'solaire-mode)
-
 ;; Enable solaire-mode anywhere it can be enabled
 (solaire-global-mode +1)
-;; To enable solaire-mode unconditionally for certain modes:
-(add-hook 'ediff-prepare-buffer-hook #'solaire-mode)
 
-;; ...if you use auto-revert-mode, this prevents solaire-mode from turning
-;; itself off every time Emacs reverts the file
-(add-hook 'after-revert-hook #'turn-on-solaire-mode)
-
-;; highlight the minibuffer when it is activated:
-(add-hook 'minibuffer-setup-hook #'solaire-mode-in-minibuffer)
-
-;; if the bright and dark background colors are the wrong way around, use this
-;; to switch the backgrounds of the `default` and `solaire-default-face` faces.
-;; This should be used *after* you load the active theme!
-;;
-;; NOTE: This is necessary for themes in the doom-themes package!
-(solaire-mode-swap-bg)
-
-;; (require 'doom-modeline)
-;; (doom-modeline-mode 1)
+(require 'doom-modeline)
+(doom-modeline-mode 1)
 ;; (setq doom-modeline-bar-width 3)
 ;; (setq doom-modeline-major-mode-color-icon nil)
 
@@ -92,10 +72,11 @@
 (setq column-number-mode t)
 ;;(set-frame-parameter (selected-frame) 'alpha '(90 . 50))
 ;;(add-to-list 'default-frame-alist '(alpha . (90 . 50)))
-(set-frame-font "Source Code Pro Semibold 10" nil t)
+(set-frame-font "Source Code Pro 10" nil t)
 
 
-;ess-mode configuration
+;;ess-mode configuration
+(require 'ess-site)
 (setq ess-ask-for-ess-directory nil) 
 (setq inferior-R-program-name "/usr/bin/R") 
 (setq ess-local-process-name "R") 
@@ -104,13 +85,126 @@
 (setq comint-scroll-to-bottom-on-output t) 
 (setq comint-move-point-for-output t)
 (setq ess-eval-visibly-p nil)
-(require 'ess-site)
+
+(with-eval-after-load 'ess-mode
+     (define-key ess-mode-map (kbd "<C-return>") nil)
+     (define-key ess-mode-map (kbd "<S-return>") 'ess-eval-line-and-step))
 
 (tooltip-mode -1)
 (setq tooltip-use-echo-area t)
 
 (elpy-enable)
 (add-hook 'after-init-hook 'global-company-mode)
+(define-key elpy-mode-map (kbd "<C-return>") nil)
+(define-key elpy-mode-map (kbd "<S-return>") 'elpy-shell-send-statement-and-step)
 
 (setq org-log-done 'time)
 (global-set-key (kbd "C-x g") 'magit-status)
+
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay        0.5
+          treemacs-directory-name-transformer      #'identity
+          treemacs-display-in-side-window          t
+          treemacs-eldoc-display                   t
+          treemacs-file-event-delay                5000
+          treemacs-file-extension-regex            treemacs-last-period-regex-value
+          treemacs-file-follow-delay               0.2
+          treemacs-file-name-transformer           #'identity
+          treemacs-follow-after-init               t
+          treemacs-expand-after-init               t
+          treemacs-git-command-pipe                ""
+          treemacs-goto-tag-strategy               'refetch-index
+          treemacs-indentation                     2
+          treemacs-indentation-string              " "
+          treemacs-is-never-other-window           nil
+          treemacs-max-git-entries                 5000
+          treemacs-missing-project-action          'ask
+          treemacs-move-forward-on-expand          nil
+          treemacs-no-png-images                   nil
+          treemacs-no-delete-other-windows         t
+          treemacs-project-follow-cleanup          nil
+          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                        'left
+          treemacs-read-string-input               'from-child-frame
+          treemacs-recenter-distance               0.1
+          treemacs-recenter-after-file-follow      nil
+          treemacs-recenter-after-tag-follow       nil
+          treemacs-recenter-after-project-jump     'always
+          treemacs-recenter-after-project-expand   'on-distance
+          treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
+          treemacs-show-cursor                     nil
+          treemacs-show-hidden-files               t
+          treemacs-silent-filewatch                nil
+          treemacs-silent-refresh                  nil
+          treemacs-sorting                         'alphabetic-asc
+          treemacs-select-when-already-in-treemacs 'move-back
+          treemacs-space-between-root-nodes        t
+          treemacs-tag-follow-cleanup              t
+          treemacs-tag-follow-delay                1.5
+          treemacs-text-scale                      nil
+          treemacs-user-mode-line-format           nil
+          treemacs-user-header-line-format         nil
+          treemacs-wide-toggle-width               70
+          treemacs-width                           35
+          treemacs-width-increment                 1
+          treemacs-width-is-initially-locked       t
+          treemacs-workspace-switch-cleanup        nil)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)
+
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple)))
+
+    (treemacs-hide-gitignored-files-mode nil))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-evil
+  :after (treemacs evil)
+  :ensure t)
+
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :hook (dired-mode . treemacs-icons-dired-enable-once)
+  :ensure t)
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :ensure t)
+
+(use-package treemacs-persp ;;treemacs-perspective if you use perspective.el vs. persp-mode
+  :after (treemacs persp-mode) ;;or perspective vs. persp-mode
+  :ensure t
+  :config (treemacs-set-scope-type 'Perspectives))
+
+
+;; maximized startup
+
+(add-hook 'window-setup-hook 'toggle-frame-maximized t)
